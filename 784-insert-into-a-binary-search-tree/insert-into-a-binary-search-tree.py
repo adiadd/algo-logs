f51e12300384fa4c7ced433b3875810840281c8a
aditya <adi.addepalli@gmail.com>
2025-02-12 23:04:51 -0500

    Time: 2 ms (40.85%) | Memory: 19.6 MB (21.87%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
@@ -13,7 +13,7 @@ class Solution:
             if val > root.val:
                 root.right = insert(root.right, val)
             elif val < root.val:
-                root.left = insert(root.left, val)
+                root.left = insert(root.left,val)
 
             return root
 
