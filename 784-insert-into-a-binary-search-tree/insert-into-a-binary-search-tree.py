660c6bdc3528538153371d492dfa90612bedd49e
aditya <adi.addepalli@gmail.com>
2026-05-08 11:28:51 -0400

    Time: 3 ms (36.51%) | Memory: 21.1 MB (53.14%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
@@ -8,16 +8,10 @@ class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         if not root:
             return TreeNode(val)
-        curr = root
-        while True:
-            if val > curr.val:
-                if not curr.right:
-                    curr.right = TreeNode(val)
-                    return root
-                curr = curr.right
-            
-            elif val < curr.val:
-                if not curr.left:
-                    curr.left = TreeNode(val)
-                    return root
-                curr = curr.left
+        
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        else:
+            root.left = self.insertIntoBST(root.left,val)
+        return root
+        
